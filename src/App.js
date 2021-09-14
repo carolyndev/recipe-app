a13d97116dba570ade76abf00790987b8f6a6e17
Carolyn Lau <thecarolynlau@gmail.com>
2021-09-13 23:02:43 -0700

    set-up basic styles

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ function App() {
   return (
     <div className="App">
       <header className="header">
-        <p>hello world</p>
+        <nav className="nav__bar">
+          <div className="nav__links">
+            <div className="nav__link">link 1</div>
+            <div className="nav__link">link 2</div>
+            <div className="nav__link">link 3</div>
+          </div>
+        </nav>
       </header>
+      <p>hello world</p>
     </div>
   );
 }
